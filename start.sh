d52a06877deba1772c81cc3755d777724d24760b
Vijeta <117478081+vijeraghu@users.noreply.github.com>
2025-03-31 00:08:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Install Python dependencies
+pip install -r requirements.txt
+
 # Start the FastAPI server
-uvicorn main:app --host 0.0.0.0 --port $PORT
+python -m uvicorn main:app --host 0.0.0.0 --port $PORT
